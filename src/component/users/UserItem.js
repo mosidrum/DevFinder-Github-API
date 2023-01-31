bbac6c151c5969abdf44d3dc2728d9a533f4a5d9
mosidrum <mosiokanga@gmail.com>
2023-02-01 00:38:20 +0100

    all components now functional components

--- src/component/users/UserItem.js
+++ src/component/users/UserItem.js
@@ -1,26 +1,28 @@
 import React from "react";
+import propTypes from "prop-types";
 
-const UserItem = (props) => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
 
-    //destructuring state
-    const { login, avatar_url, html_url } =props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: propTypes.object.isRequired,
+};
 
 export default UserItem;
