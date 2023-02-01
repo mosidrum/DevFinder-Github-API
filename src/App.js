d01eb1baafa123f92f961fc4ab9f2f457ae67a56
mosidrum <mosiokanga@gmail.com>
2023-02-01 08:09:30 +0100

    Done with fetching users from github API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import React, { Component } from "react";
 import Navbar from "./component/layout/Navbar";
 import Users from "./component/users/Users";
+import Search from "./component/users/Search";
+import Alert from "./component/layout/Alert";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear Users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(()=> this.setState({alert:null}), 2500)
+  };
 
   render() {
+    const { users, loading, } = this.state;
     return (
       <nav className='App'>
         <Navbar />
-          <div className="container">
-            <Users />
-          </div>
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+        </div>
       </nav>
     );
   }
